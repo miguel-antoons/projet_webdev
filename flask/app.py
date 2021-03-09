3e018d623b35bea42e1ce67d5e8deaa411cd52a9
Maxime <max@interplanet.be>
2021-03-09 21:45:44 +0100

    add mysql

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import json
+import time
 
 app = Flask(__name__)
 # CORS(app)
