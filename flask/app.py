42c6898aaa23d9f2e2c4d037aa32c9b8d4414480
Maxime <max@interplanet.be>
2021-03-09 20:50:06 +0100

    add mysql

--- flask/app.py
+++ flask/app.py
@@ -1,19 +1,39 @@
-from flask import Flask
-from flask_cors import CORS
-import time
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 # CORS(app)
 
+app.secret_key = 'your secret key'
+
+app.config['MYSQL_HOST'] = 'sql11.freemysqlhosting.net'
+app.config['MYSQL_USER'] = 'sql11397267'
+app.config['MYSQL_PASSWORD'] = '7tPK1krCUh'
+app.config['MYSQL_DB'] = 'sql11397267'
+
+mysql = MySQL(app)
+
 
 @app.route('/')
 def index():
-    return Flask.render_template('index.html')
+    cur = mysql.connection.cursor()
+    # cur.execute('''create table user (id MEDIUMINT NOT NULL AUTO_INCREMENT,nom 	varchar(12) NULL, prenom varchar(12) NULL, PRIMARY KEY(id) );''')
+
+    # cur.execute('''create table hello (id INTEGER,nom varchar(12), prenom varchar(12), PRIMARY KEY(id) );''')
+
+    cur.execute('''INSERT INTO user VALUES (3,'Danlee', 'Maxime');''')
+
+    cur.execute('''SELECT * from user''')
+    results = cur.fetchall()
+    print(results)
+    mysql.connection.commit()
+    return 'DONE!'
 
 
 @app.route('/hello')
 def hello():
     return {'result': "HELLO"}
+
 
 @app.route('/api/time')
 def get_current_time():
