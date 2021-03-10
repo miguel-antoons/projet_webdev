56725f09a2e706c2cadb754f270c5475f9a6bb68
Maxime <max@interplanet.be>
2021-03-10 15:03:49 +0100

    test app.js / index.html / index.js

--- react_code/src/App.js
+++ react_code/src/App.js
@@ -1,35 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const [placeholder, setPlaceholder] = useState('Hi');
 
-  useEffect(() => {
-    fetch('/hello').then(res => res.json()).then(data => {
-      setPlaceholder(data.result);
-    });
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Flask says {placeholder}</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
--- react_code/src/index.js
+++ react_code/src/index.js
@@ -4,14 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const myelement = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(myelement, document.getElementById('root'));
+
 reportWebVitals();
