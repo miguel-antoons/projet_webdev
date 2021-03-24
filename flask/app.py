5e2e7212575960fb7b1857cfbde0e66deca709af
BentahriMerwane <64090979+BentahriMerwane@users.noreply.github.com>
2021-03-24 11:31:56 +0100

    Page d'enregistrement client
    Page d'enregistrement client finis

--- flask/app.py
+++ flask/app.py
@@ -8,17 +8,23 @@ app = Flask(__name__)
 
 app.secret_key = 'your secret key'
 
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Deb-Web1234'
+app.config['MYSQL_PASSWORD'] = 'Dev-Web1234'
 app.config['MYSQL_DB'] = 'projet-dev'
 mysql = MySQL(app)
+
+
+
+
+    
+
+    
 
 
 @app.route('/')
 def index():
     cur = mysql.connection.cursor()
-    # cur.execute('''create table user (id MEDIUMINT NOT NULL AUTO_INCREMENT,nom 	varchar(12) NULL, prenom varchar(12) NULL, PRIMARY KEY(id) );''')
 
     # cur.execute('''create table hello (id INTEGER,nom varchar(12), prenom varchar(12), PRIMARY KEY(id) );''')
 
@@ -42,19 +48,62 @@ def index():
 def hello():
     return {'result': "HELLO"}
 
-@app.route('/api/test', methods=['POST'])
-def post():
+@app.route('/api/enregistrement_client', methods=['POST','GET'])
+def save_client():
+
+    
     print('hello')
     record = json.loads(request.data)
+    print(record)
+    nom =  record['name']
     prenom = record['firstName']
-    nom = record['name']
+    titre = record["titre"]
+    societe = record["societe"]
+    langue = record["langue"]
+    adress = record['adress']
+    tva = record['tva']
+    number = record['number']
+    email = record['email']
+    
+    print(nom)
+    print(prenom)
+    print(titre)
+    print(societe)
+    print(langue)
 
-    mysql = MySQL(app)
+
+    #Creating a connection cursor
     cursor = mysql.connection.cursor()
-    cursor.execute(''' INSERT INTO info_table VALUES(%s,%s)''',(nom,prenom,'fr'))
+
+    """
+    cur.execute('''create CREATE  TABLE test- Projet (
+        client_ID INT NOT NULL AUTO_INCREMENT ,
+        client_NAME VARCHAR(45) NOT NULL , 
+        client_SURNAME VARCHAR(45) NOT NULL ,
+        client_TITLE CHAR(3) NOT NULL ,
+        client_COMPANY VARCHAR(45) NOT NULL ,
+        client_ADRESS VARCHAR(150) NULL ,
+        client_TVA VARCHAR(45) NULL ,
+        client_LANGUAGE VARCHAR(3) NOT NULL ,
+        client_NAMEARCHITECTE VARCHAR(45) NULL ,
+        client_NUMBER VARCHAR(45) NULL ,
+        client_COMMENT VARCHAR(45) NULL ,
+        PRIMARY KEY (client_ID) );''')
+    
+    """
+
+    #Executing SQL Statements
+   
+    cursor.execute(''' INSERT INTO client_table (client_NAME,client_SURNAME,client_TITLE,client_COMPANY,client_LANGUAGE,client_ADRESS,client_TVA,client_NUMBER,client_MAIL)
+     VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s) ''',(nom,prenom,titre,societe,langue,adress,tva,number,email))
+   
+    
+    #Saving the Actions performed on the DB
     mysql.connection.commit()
+    
+    #Closing the cursor
     cursor.close()
- 
+
     return record
 
 
--- react_code/src/components/Pages/Ajout_client/Ajout_client.js
+++ react_code/src/components/Pages/Ajout_client/Ajout_client.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import  { useState } from 'react';
+import * as BS from "react-bootstrap"
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
 
 // ici le component s'appelle home puisque je travaillais dessus, ça n'a pas d'importance et chez toi il s'apellera probablemnt autrement
 const Client = () => {
     // Met le state des inputs
     const [name, setName] = useState('');
     const [firstName, setFirstName] = useState('');
-  
+    const [titre,setTitre] = useState('Mr');
+    const [societe,setSociete] = useState('');
+    const [langue,setLangue] = useState('FR');
+    const [adress,setAdress] = useState('');
+    const [tva,setTva] = useState('');
+    const [number,setNumber] = useState('');
+    const [email,setEmail] = useState('');
+    
+
+    
+    
+
     // envoi les données des inputs onSubmit
     const sendPost = async (event) => {
       
       // évite que la page se recharge onSubmit du formulaire
       event.preventDefault();
-  
+
       try{
-        let result = await fetch('/api/test', {
+        let result = await fetch('/api/enregistrement_client', {
           method: 'post',
           //mode: 'no-cors', // --> pas besoin de cette ligne
           headers: {
             //'Accept': 'application/json', // --> pas besoin de cette ligne non-plus
             'Content-type': 'application/json',
           },
-          body: JSON.stringify( {name, firstName} ) // l'objet à l'intérieur de la fonction contient l'état des 2 input
+          body: JSON.stringify( {name, firstName, societe, titre, langue,adress,tva,number,email} ) // l'objet à l'intérieur de la fonction contient l'état des 5 input
         });
   
         const data = await result.json();
@@ -31,16 +48,80 @@ const Client = () => {
       catch(e){
         console.log(e);
       }
+     
     };
   
     return (
-      <form onSubmit={ (event) => { sendPost(event); }}>
-        <label>Nom</label>
-        <input type='text' value={ name } onChange={ (e) => setName(e.target.value) }></input>
-        <label>Prénom</label>
-        <input type='text' value={ firstName } onChange={ (e) => setFirstName(e.target.value) }></input>
-        <input type='submit' value='SAVE' />
-      </form>
+
+     
+
+        <BS.Container>
+
+      <BS.Form onSubmit={ (event) => { sendPost(event); }}  >
+
+
+        <BS.Form.Group  >
+          <BS.Form.Label column="lg" lg={2}  class="col-auto col-form-label" >Nom</BS.Form.Label>
+          <BS.Form.Control size="sm" type="text" placeholder="Entrer le nom du client"  id="name" name="name" 
+          value={ name } onChange={ (e) => setName(e.target.value)   } required />
+
+          <BS.Form.Label column="lg" lg={2} >Prénom</BS.Form.Label>
+          <BS.Form.Control size="sm" type="text" placeholder="Entrer le Prénom du client"  id="firstName " name="firstName " 
+          value={ firstName } onChange={ (e) => setFirstName(e.target.value)   } required />
+        </BS.Form.Group>
+
+        <BS.Form.Group>
+          <BS.Form.Label column="lg" lg={2} >Société</BS.Form.Label>
+          <BS.Form.Control size="sm" type="text" placeholder="Entrer la Société du client"  id="societe " name="societe" 
+          value={ societe } onChange={ (e) => setSociete(e.target.value)  } required />
+
+          <BS.Form.Label column="lg" lg={2} >Adresse</BS.Form.Label>
+          <BS.Form.Control size="sm" type="text" placeholder="Entrer l'adresse du client"  id="adress" name="adress " 
+          value={ adress } onChange={ (e) => setAdress(e.target.value)   }  />
+
+        <BS.Form.Label column="lg" lg={2} >N°TVA</BS.Form.Label>
+          <BS.Form.Control size="sm" type="text" placeholder="Entrer le N° TVA"  id="tva" name="tva" 
+          value={ tva } onChange={ (e) => setTva(e.target.value)   }  />
+
+        <BS.Form.Label column="lg" lg={2} >Numéro</BS.Form.Label>
+          <BS.Form.Control size="sm" type="text" placeholder="Entrer le numéro du client" maxLength='10'  id="number" name="number" 
+          value={ number } onChange={ (e) => setNumber(e.target.value)   }  />
+
+          <BS.Form.Label column="lg" lg={2} >Email</BS.Form.Label>
+          <BS.Form.Control size="sm" type="text" placeholder="Entrer l'émail du client"  id="email" name="email" 
+          value={ email } onChange={ (e) => setEmail(e.target.value)   }  />
+
+        </BS.Form.Group>
+
+        <BS.Form.Group>
+          <BS.Form.Label column="lg" lg={2} >Titre</BS.Form.Label>
+          <BS.Form.Control size="sm" as="select" id="titre" name="titre" value={titre}  onChange={ (e) => setTitre(e.target.value)  }   required>
+          <option value="Mr">Mr</option>
+          <option value="Mme">Mme</option>
+
+          </BS.Form.Control>
+        </BS.Form.Group>
+
+        <BS.Form.Group>
+          <BS.Form.Label column="lg" lg={2} >Langue</BS.Form.Label>
+          <BS.Form.Control size="sm" as="select" name="langue" id="langue" value={langue}  onChange={ (e) => setLangue(e.target.value)  }required>
+          <option value="FR">FR</option>
+          <option value="NDL">NDL</option>
+          </BS.Form.Control>
+
+         
+        </BS.Form.Group>
+
+
+
+          <BS.Button className="submite" variant="info" type="submit" >Enregistrer Client</BS.Button> 
+
+      </BS.Form>
+
+      </BS.Container>
+
+      
+
     );
   };
 
