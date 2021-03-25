eb386dbda56667dadead27e8b7dc2cf52a84db9c
Miguel-Antoons <miguel.antoons@gmail.com>
2021-03-25 21:25:24 +0100

    Ajout d'éléments à la page qui listera les projets

--- react_code/src/components/Pages/Devis/ProjetsDevis.js
+++ react_code/src/components/Pages/Devis/ProjetsDevis.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as icon from 'react-icons/io5';
 import * as BS from "react-bootstrap";
+import { Filter } from '../../ProjetListe/Filter.js'
 import { recherche, filtre } from './ProjetsDevis.css';
 
 
@@ -10,10 +11,10 @@ function ProjetsDevis () {
             <BS.Jumbotron>
                 <h1>Bienvenue dans Devis</h1>
                 <div className="d-flex justify-content-center">
-                    <BS.Col lg="2"></BS.Col>
+                    <BS.Col lg="2" xs></BS.Col>
                     <BS.Col md="auto">
                         <input type="text" className="form-control form-control-lg recherche" placeholder="Rechercher . . ." />
-                        <select className="form-control form-control-lg filtre">
+                        <select className="form-control form-control-lg shadow-none bg-transparent filtre">
                             <option>A-Z Client</option>
                             <option>A-Z Chantier</option>
                             <option>Z-A Client</option>
@@ -21,8 +22,9 @@ function ProjetsDevis () {
                             <option>Récent à Ancien</option>
                             <option>Ancien à Récent</option>
                         </select>
+                        <Filter />
                     </BS.Col>
-                    <BS.Col lg="2"></BS.Col>
+                    <BS.Col lg="2" xs></BS.Col>
                 </div>
             </BS.Jumbotron>     
         </BS.Container>
---
+++ react_code/src/components/ProjetListe/Filter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as BS from "react-bootstrap";
+
+
+const FilterProject = () => {
+
+    return (
+        <BS.Form.Check type='radio'>
+            <BS.Form.Check.Input type='radio'></BS.Form.Check.Input>
+            <BS.Form.Check.Label>Test</BS.Form.Check.Label>
+        </BS.Form.Check>
+    );
+};
+
+export default Filter;
