f7e86aaedd888178a33bdffd6bd0bea472334587
Miguel-Antoons <miguel.antoons@gmail.com>
2021-03-31 11:05:09 +0200

    Avance sur le design de la page regroupnt l'ensemble des devis

--- react_code/src/components/ProjetListe/Filter.js
+++ react_code/src/components/ProjetListe/Filter.js
@@ -5,10 +5,20 @@ import * as BS from "react-bootstrap";
 const Filter = () => {
 
     return (
-        <BS.Form.Check type='radio'>
-            <BS.Form.Check.Input type='radio'></BS.Form.Check.Input>
-            <BS.Form.Check.Label>Test</BS.Form.Check.Label>
-        </BS.Form.Check>
+        <BS.ButtonGroup>
+            <div className='form-check'>
+                <input className="form-check-input" type="radio" name="sortTable" id="3_mois" value="mois" defaultChecked />
+                <label className="form-check-label" for="exampleRadios1">test</label>
+            </div>
+            <div className='form-check'>
+                <input className="form-check-input" type="radio" name="sortTable" id="1_an" value="an"/>
+                <label className="form-check-label" for="exampleRadios1">test</label>
+            </div>
+            <div className='form-check'>
+                <input className="form-check-input" type="radio" name="sortTable" id="tout" value="tout"/>
+                <label className="form-check-label" for="exampleRadios1">test</label>
+            </div>
+        </BS.ButtonGroup>
     );
 };
 
