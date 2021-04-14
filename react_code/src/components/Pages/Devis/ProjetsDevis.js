e03b3c8eb5de45d57cfc993962fe50abe549d4d1
Miguel-Antoons <miguel.antoons@gmail.com>
2021-04-14 22:06:01 +0200

    ajout de la fonction qui permet de rechercher un projet à l'aide d'une barre de recherche

--- react_code/src/components/Pages/Devis/ProjetsDevis.js
+++ react_code/src/components/Pages/Devis/ProjetsDevis.js
@@ -8,32 +8,46 @@ import { LinkContainer } from 'react-router-bootstrap';
 
 
 function ProjetsDevis () {
+    const [sort, setSort] = useState('{"key": "date", "sign": 0}');
+    const [search, setSearch] = useState('');
+    const [shownProjects, setShownProjects] = useState([]);
     const three_months_ago = new Date();
     three_months_ago.setMonth(three_months_ago.getMonth() - 3);
     const [filter, setFilter] = useState(three_months_ago);
     const [projects, setProjects] = useState([]);
 
-
+    // GET information about all projects, fires on page load and on filter change
     useEffect(() => {
+        /**
+         * Function call the API which responds wuth the basic information about
+         * all the projects that the filter lets trough.
+         * In case an error occured, the error is printed in console.
+         */
         const fetchProjects = async () => {
-            const response = await fetch('/api/devis?filter='+ filter);
-            const data = await response.json();
-            setProjects(data);
+            try {
+                const response = await fetch('/api/devis?filter=' + filter);
+                const data = await response.json();
+                setProjects(data);
+                setShownProjects(data);
+            }
+            catch (e) {
+                console.log(e);
+            }
         }
 
         fetchProjects();
     }, [filter]);
-
+    
 
     /**
-     * Function fires when the sort option changes.
-     * The function will sort the projects according to the value chosen by the user
-     * 
-     * @param {*} event the event that launched the function
+     * Function sorts the array with all the projects according to the users 
+     * choice and afterwards calls a function to filter the sorted result according
+     * to the user's choice (again).
+     * @param {Array} arrayToSort array to sort and filter
      */
-    function sortProjects(event){
-        let values = JSON.parse(event.target.value);        // get values from html element
-        let sortedProjects = projects.slice();              // Create new array from the state array
+    const sortProjects = (arrayToSort) => {
+        let values = JSON.parse(sort);        // get values from html element
+        let sortedProjects = arrayToSort.slice();      // Create new array from the state array
 
         // Function which converts string dates (i.e. 18th April 2020) to ISO date format
         const stringToDate = (stringDate) => {
@@ -101,8 +115,56 @@ function ProjetsDevis () {
             });
         }
         
-        setProjects(sortedProjects);        // change the projects order in the actual state
+        setShownProjects(filterProjects(sortedProjects));        // change the (filtered) projects order in the actual state
     };
+
+
+    /**
+     * Function fires when the sort option changes.
+     * The function will sort the projects according to the value chosen by the user
+     */
+    useEffect( () => {
+        try {
+            sortProjects(projects);
+        }
+        catch (e) {
+            console.log(e);
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [sort]);
+
+
+    /**
+     * function filters an array depending on what is written in the
+     * search bar. The content written in the search bar is memorized
+     * in the 'search' state variable.
+     * 
+     * @param {Array} unfilteredArray 
+     * @returns filtered array
+     */
+    const filterProjects = (unfilteredArray) => {
+        return unfilteredArray.filter(project => {
+            return (
+                (String(project["id"]).toLowerCase().indexOf(search) > -1) || 
+                (project["name"].toLowerCase().indexOf(search) > -1) || 
+                (project["chantier"].toLowerCase().indexOf(search) > -1) || 
+                (project["date"].toLowerCase().indexOf(search) > -1)
+            );
+        })
+    };    
+
+
+    // Fires when the user is trying to search a project
+    useEffect(() => {
+        try {
+            sortProjects(projects);
+        }
+        catch (e) {
+            console.log(e);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [search])
 
 
     return (
@@ -118,8 +180,8 @@ function ProjetsDevis () {
                 <div className="d-flex justify-content-center">
                     <BS.Col lg="2" xs></BS.Col>
                     <BS.Col md="auto">
-                        <input type="text" className="form-control form-control-lg recherche" placeholder="Rechercher . . ." />
-                        <select onChange={sortProjects} className="form-control form-control-lg shadow-none bg-transparent filtre">
+                        <input onChange={ (e) => setSearch(e.target.value) } value={ search } type="text" className="form-control form-control-lg recherche" placeholder="Rechercher . . ." />
+                        <select onChange={(event) => setSort(event.target.value)} className="form-control form-control-lg shadow-none bg-transparent filtre">
                             <option value='{"key": "date", "sign": 0}' key="newOld">Récent à Ancien</option>
                             <option value='{"key": "date", "sign": 1}' key="oldNew">Ancien à Récent</option>
                             <option value='{"key": "name", "sign": 0}' key="AZClient">A-Z Client</option>
@@ -143,7 +205,7 @@ function ProjetsDevis () {
                     </tr>
                 </thead>
                 <tbody>
-                    <TableauProjets projects={ projects } />
+                    <TableauProjets projects={ shownProjects } />
                 </tbody>
             </BS.Table> 
         </BS.Container>
