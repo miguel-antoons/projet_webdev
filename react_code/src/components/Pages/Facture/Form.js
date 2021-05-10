c0329c1e18c6a5cab587b7f508a58e4c6306f881
Maxime <max@interplanet.be>
2021-05-10 15:20:53 +0200

    add function print

--- react_code/src/components/Pages/Facture/Form.js
+++ react_code/src/components/Pages/Facture/Form.js
@@ -190,7 +190,7 @@ class Form extends Component {
                 </BS.Form.Group>
                 <Preview state={this.props.returnState} />
                 <BS.Button id="save" className="no-print mb-2 ml-2" variant="info" onClick={this.save}>Sauvegarder</BS.Button> {' '}
-                <BS.Button className="no-print mb-2 ml-2" variant="info" onClick={this.print()}>Imprimer</BS.Button> {' '}
+                <BS.Button className="no-print mb-2 ml-2" variant="info" onClick={this.print}>Imprimer</BS.Button> {' '}
                 <div id="msg"></div>
 
             </BS.Form>
