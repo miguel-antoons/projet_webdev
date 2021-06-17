87f9f73bc0277b3d27453b8027463a81024f42a8
Miguel-Antoons <miguel.antoons@gmail.com>
2021-06-17 21:56:21 +0200

    amélioration du style de la page client

--- react_code/src/components/Pages/Ajout_client/Ajout_client.js
+++ react_code/src/components/Pages/Ajout_client/Ajout_client.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import  { useState } from 'react';
-import * as BS from "react-bootstrap"
-// import { MDBInput } from "mdbreact";
+import * as BS from "react-bootstrap";
 import MDBInput from '@material-ui/core/TextField';
 import Select from "react-select";
+import './ajoutClients.css';
 
 
 // ici le component s'appelle home puisque je travaillais dessus, ça n'a pas d'importance et chez toi il s'apellera probablemnt autrement
@@ -11,13 +11,23 @@ const Ajout_Client = (props) => {
     // Met le state des inputs
     const [name, setName] = useState('');
     const [firstname, setFirstname] = useState('');
-    const [titre,setTitre] = useState('Mr');
-    const [societe,setSociete] = useState('');
-    const [langue,setLangue] = useState('FR');
-    const [adress,setAdress] = useState('');
-    const [tva,setTva] = useState('');
-    const [number,setNumber] = useState('');
-    const [email,setEmail] = useState('');
+    const [titre, setTitre] = useState({value: 0, label: 'Mr'});
+    const [societe, setSociete] = useState('');
+    const [langue, setLangue] = useState({value: 0, label: 'Nl'});
+    const [adress, setAdress] = useState('');
+    const [tva, setTva] = useState('');
+    const [number, setNumber] = useState('');
+    const [email, setEmail] = useState('');
+
+    const optionsTitle = [
+        {value: 0, label: 'Mr'},
+        {value: 1, label: 'Mme'}
+    ];
+
+    const optionsLanguage = [
+        {value: 0, label: 'Nl'},
+        {value: 1, label: 'Fr'}
+    ];
 
     // envoi les données des inputs onSubmit
     const sendPost = async (event) => {
@@ -76,58 +86,34 @@ const Ajout_Client = (props) => {
     }   
   
     return (
-        <BS.Container>
-            <BS.Form onSubmit={ (event) => { sendPost(event); }}  >
-                <BS.Form.Label column="lg" lg={2} >Titre</BS.Form.Label>
-                <MDBInput
-                    label="test"
-                    type="radio"
-                    id="exampleRadios2"
-                    name="exampleRadios"
-                /><br />
-
-
-
-                <MDBInput label="Nom" background size="lg" value={ name } onChange={ (e) => setName(e.target.value) } />
-                <MDBInput label="Prenom" value={ firstname } onChange={ (e) => setFirstname(e.target.value) } required />
-
-                <MDBInput label="Société" value={ societe } onChange={ (e) => setSociete(e.target.value)  } required />
-
-                <MDBInput size="lg" label="Adresse 1" value={ adress } onChange={ (e) => setAdress(e.target.value) }  />
-                <MDBInput size="lg" label="Adresse 2" value={ adress } onChange={ (e) => setAdress(e.target.value) }  />
-
-                <MDBInput size="lg" label="N° de TVA" value={ tva } onChange={ (e) => setTva(e.target.value)   }  />
-
-                <MDBInput size="lg" label="Téléphone" value={ number } onChange={ (e) => setNumber(e.target.value)   }  />
-
-                <MDBInput size="lg" label="E-mail" value={ email } onChange={ (e) => setEmail(e.target.value)   }  />
-
-                <BS.Form.Control size="sm" as="select" id="titre" name="titre" value={titre}  onChange={ (e) => setTitre(e.target.value)  }   required>
-                <option value="Mr">Mr</option>
-                <option value="Mme">Mme</option>
-
-                </BS.Form.Control><br />
-
-                <Select
-                    defaultValue="Mr"
-                    label="Titre"
-                    options={["Mr", "Mme"]}
-                 />
-
-
-                <BS.Form.Label column="lg" lg={2} >Langue</BS.Form.Label>
-                <BS.Form.Control size="sm" as="select" name="langue" id="langue" value={langue} onChange={ (e) => setLangue(e.target.value)  } required>
-                <option value="FR">fr</option>
-                <option value="NL">nl</option>
-                </BS.Form.Control>
-
-                <BS.Button className="" variant="info" type="submit">Enregistrer</BS.Button> 
-            </BS.Form>
-        </BS.Container>
-
-      
-
+            <BS.Col className="mx-auto no-margin" lg='4' xs='12' align='center'>
+                <BS.Form onSubmit={ (event) => { sendPost(event); }}>
+                    <MDBInput className="defaultInput" label="Nom" value={ name } onChange={ (e) => setName(e.target.value) } />
+                    <MDBInput className="defaultInput" label="Prenom" value={ firstname } onChange={ (e) => setFirstname(e.target.value) } required />
+                    <MDBInput className="defaultInput" label="Société" value={ societe } onChange={ (e) => setSociete(e.target.value)  } required />
+                    <MDBInput className="defaultInput" label="Adresse 1" value={ adress } onChange={ (e) => setAdress(e.target.value) }  />
+                    <MDBInput className="defaultInput" label="Adresse 2" value={ adress } onChange={ (e) => setAdress(e.target.value) }  />
+                    <MDBInput className="defaultInput" label="N° de TVA" value={ tva } onChange={ (e) => setTva(e.target.value)   }  />
+                    <MDBInput className="defaultInput" label="Téléphone" value={ number } onChange={ (e) => setNumber(e.target.value)   }  />
+                    <MDBInput className="defaultInput" label="E-mail" value={ email } onChange={ (e) => setEmail(e.target.value)   }  />
+                    <Select
+                        className="defaultSelect border-0"
+                        defaultValue={titre}
+                        onChange={(event) => setTitre(event)}
+                        label="Titre"
+                        options={optionsTitle}
+                    />
+                    <Select
+                        className="defaultSelect"
+                        defaultValue={langue}
+                        onChange={ (event) => setLangue(event) }
+                        label="Titre"
+                        options={optionsLanguage}
+                    />
+                    <BS.Button className="" variant="info" type="submit">Enregistrer</BS.Button> 
+                </BS.Form>
+            </BS.Col>
     );
-  };
+};
 
 export default Ajout_Client ;
