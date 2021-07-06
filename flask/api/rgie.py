1f281650fcfe70384f59d61607c3a6ca6b18255d
Miguel-Antoons <miguel.antoons@gmail.com>
2021-07-06 22:13:22 +0200

    isolation des différents methodes d'API dans des v)fonction individuelles pour la fonctionnalité régie

--- flask/api/rgie.py
+++ flask/api/rgie.py
@@ -13,45 +13,9 @@ def rgie():
 
     # API will return all the projects
     if request.method == 'GET':
-        # get the filter to apply to the projects
-        filter = request.args.get('filter')
-        arguments = ()
-
-        # prepare the sql statement (which contains arguments in order to
-        # avoid sql injection)
-        sql_procedure = """
-            SELECT R.ID_LISTE_ARTICLE_RGIE, C.NOM_CLIENT, C.PRENOM_CLIENT,
-                C.SOCIETE_CLIENT, R.CHANTIER,
-                date_format(R.DATE, '%%D %%M %%Y')
-            FROM rgie as R
-                join clients as C on R.ID_CLIENT = C.ID_CLIENT
-            WHERE R.DATE >= str_to_date(%s, %s)
-                and extract(year from R.DATE) >= %s
-                and extract(year from R.DATE) <= %s
-            ORDER BY R.DATE desc"""
-
         # complete arguments according to the filter
-        if len(filter) > 4:
-            arguments = (filter, "%a %b %e %Y %H:%i:%s", 1, 999999999, )
-        elif len(filter) == 4:
-            arguments = ("1999-01-01", "%Y-%m-%d", filter, filter, )
-        else:
-            arguments = ("1999-01-01", "%Y-%m-%d", 1, 999999999, )
-
-        # execute the statement
-        cur.execute(sql_procedure, arguments)
-
-        # fetch the result
-        results = cur.fetchall()
-
-        # turn the result into a list of dictionnaries
-        for row in results:
-            response.append({
-                'id': row[0],
-                'attribute1': f"{row[1]} {row[2]}, {row[3]}",
-                'attribute2': row[4],
-                'date': row[5]
-            })
+        # then get all etiquettes and store them into the response variable
+        response = get_all_rgie(set_arguments(request.args.get('filter')), cur)
 
     elif request.method == 'DELETE':
         # get the id of the projects that has to be deleted
@@ -75,3 +39,46 @@ def rgie():
     cur.close()
 
     return json.dumps(response)
+
+
+def get_all_rgie(arguments, cursor):
+    response = []
+
+    # prepare the sql statement (which contains arguments in order to
+    # avoid sql injection)
+    sql_procedure = """
+        SELECT R.ID_LISTE_ARTICLE_RGIE, C.NOM_CLIENT, C.PRENOM_CLIENT,
+            C.SOCIETE_CLIENT, R.CHANTIER,
+            date_format(R.DATE, '%%D %%M %%Y')
+        FROM rgie as R
+            join clients as C on R.ID_CLIENT = C.ID_CLIENT
+        WHERE R.DATE >= str_to_date(%s, %s)
+            and extract(year from R.DATE) >= %s
+            and extract(year from R.DATE) <= %s
+        ORDER BY R.DATE desc"""
+
+    cursor.execute(sql_procedure, arguments)
+
+    # fetch the result
+    results = cursor.fetchall()
+
+    # turn the result into a list of dictionnaries
+    for row in results:
+        response.append({
+            'id': row[0],
+            'attribute1': f"{row[1]} {row[2]}, {row[3]}",
+            'attribute2': row[4],
+            'date': row[5]
+        })
+
+    return response
+
+
+def set_arguments(filter):
+    # complete arguments according to the filter
+    if len(filter) > 4:
+        return (filter, "%a %b %e %Y %H:%i:%s", 1, 999999999, )
+    elif len(filter) == 4:
+        return ("1999-01-01", "%Y-%m-%d", filter, filter, )
+    else:
+        return ("1999-01-01", "%Y-%m-%d", 1, 999999999, )
